44bfda414ca67e430a8756cc80db1cab647772da
Konn25 <koncz.bence9@gmail.com>
2023-06-22 11:21:38 +0200

    create controller

---
+++ src/main/java/com/billing/ElectricityBillingSystem/controller/ClientController.java
@@ -0,0 +1,69 @@
+package com.billing.ElectricityBillingSystem.controller;
+
+import com.billing.ElectricityBillingSystem.dto.ClientDTO;
+import com.billing.ElectricityBillingSystem.jpa.Client;
+import com.billing.ElectricityBillingSystem.jpa.Meter;
+import com.billing.ElectricityBillingSystem.service.ClientService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/v1")
+@RequiredArgsConstructor
+public class ClientController {
+
+    private final ModelMapper modelMapper;
+
+    private final ClientService clientService;
+
+    @PostMapping("/createclient")
+    @ResponseBody
+    public ResponseEntity<String> createClient(@RequestBody ClientDTO clientDTO){
+
+        Client clientRequest = modelMapper.map(clientDTO,Client.class);
+
+        List<Client> clientList = clientService.getAllClient();
+
+       for(Client value: clientList){
+            if(value.getPostalCode()==clientRequest.getPostalCode() && value.getCity().equals(clientRequest.getCity()) &&
+                    value.getStreet().equals(clientRequest.getStreet()) && value.getHouseNumber() == clientRequest.getHouseNumber()){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("This address is already registered!");
+            }
+        }
+
+        Client newClient = clientService.createClient(clientRequest);
+
+        ClientDTO clientResponse = modelMapper.map(newClient, ClientDTO.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(String.valueOf(clientResponse));
+    }
+
+
+    @GetMapping("/clients")
+    @ResponseBody
+    public List<Client> getAllClient(){
+        return clientService.getAllClient();
+    }
+
+    @GetMapping("/client/meter/{clientId}")
+    @ResponseBody
+    public Meter getClientMeter(@PathVariable(name = "clientId") Long clientId){
+        return clientService.getClientMeter(clientId);
+    }
+
+    @GetMapping("/client/find/{clientId}")
+    @ResponseBody
+    public Optional<Client> findClientById(@PathVariable(name = "clientId") Long clientId){
+        return clientService.findClientById(clientId);
+    }
+
+
+}
---
+++ src/main/java/com/billing/ElectricityBillingSystem/controller/MeterController.java
@@ -0,0 +1,41 @@
+package com.billing.ElectricityBillingSystem.controller;
+
+import com.billing.ElectricityBillingSystem.jpa.ClientRepository;
+import com.billing.ElectricityBillingSystem.jpa.Meter;
+import com.billing.ElectricityBillingSystem.service.ClientService;
+import com.billing.ElectricityBillingSystem.service.MeterService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/v1")
+@RequiredArgsConstructor
+public class MeterController {
+
+    private final MeterService meterService;
+
+    private final ClientService clientService;
+    private final ClientRepository clientRepository;
+
+    @PostMapping("/meter/create/{clientId}")
+    @ResponseBody
+    public ResponseEntity<?> createNewMeter(@PathVariable(value = "clientId") Long clientId) {
+
+        Meter newMeter = meterService.createNewMeter(clientId);
+
+        if(clientService.findClientById(clientId).isPresent() && clientService.findClientById(clientId).get().getMeterId()==0){
+            clientService.findClientById(clientId).get().setMeterId(newMeter.getId());
+            clientRepository.save(clientService.findClientById(clientId).get());
+
+            return ResponseEntity.status(HttpStatus.CREATED).body("Created:\n"+(Map.of("id",newMeter.getId()," customerId", newMeter.getClientId())));
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Can not update client's meter id");
+        }
+    }
+
+}
