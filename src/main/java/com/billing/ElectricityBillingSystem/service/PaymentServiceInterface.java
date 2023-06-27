7a80b3cdc4f5a88aefc727e05b4f5ba22e7da6eb
Konn25 <koncz.bence9@gmail.com>
2023-06-27 11:34:53 +0200

    modify method

--- src/main/java/com/billing/ElectricityBillingSystem/service/PaymentServiceInterface.java
+++ src/main/java/com/billing/ElectricityBillingSystem/service/PaymentServiceInterface.java
@@ -3,14 +3,14 @@ package com.billing.ElectricityBillingSystem.service;
 import com.billing.ElectricityBillingSystem.jpa.Payment;
 
 import java.util.List;
-import java.util.Optional;
+
 
 public interface PaymentServiceInterface {
 
 
     Payment createNewPayment(Payment payment);
 
-    Optional<Payment> getClientAllPayment(Long clientId);
+    List<Payment> getClientAllPayment(Long clientId);
 
     Payment clientPayingBill(Long clientId, Long paymentId);
 
